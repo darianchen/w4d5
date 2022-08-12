6ca3d141587c3cd3cdea797d3c1b4b909fd204cf
darianchen <darianychen@gmail.com>
2022-08-12 12:34:25 -0400

    phase_1 complete

--- time_algo.rb
+++ time_algo.rb
@@ -1,13 +1,8 @@
-def my_min_phase_1(arr)
-  min = nil
-  arr.each_with_index do |ele1, i| #this is changing ele1
-    arr.each_with_index do |ele2, j|
-      if ele2 < ele1
-        next
-      end
-    end
+def my_min_phase_1(arr) 
+  arr.each do |el1|
+  # return el1 if arr.none? { |el2| el1 > el2 }
+   return el1 if arr.all? { |el2| el1 <= el2 } #doesn't work with a duplicate
   end
-  return min
 end
 
 list = [ 0, 3, 5, 4, -5, 10, 1, 90]
