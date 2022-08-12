805ec7d9dc7d8334f9d6ea6cec353a47b0f84ae8
darianchen <darianychen@gmail.com>
2022-08-12 14:52:55 -0400

    anagram phase 2 complete

--- anagrams.rb
+++ anagrams.rb
@@ -1,13 +1,31 @@
 #phase 1
 
-def anagram_phase1?(str1, str2)
+def first_anagram?(str1, str2)
   str1.chars.permutation.to_a.include?(str2.chars)
 end
 
 #phase2
-def anagram(str1, str2)
-
+def anagram?(str1, str2)
+  arr = str2.chars
+  str1.chars.each do |el|
+    if arr.include?(el)
+      index = arr.find_index(el)
+      arr.delete_at(index)
+    else
+      return false
+    end
+  
+  end
+  if arr.empty?
+    return true
+  else
+    false
+  end
 end
+
+
+
+
 
 
 p anagram?("gizmo", "sally")    #=> false
