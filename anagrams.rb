a332b8286a1f132679102c254fc520c13be3c28f
sungyotkim <sungyotkim@gmail.com>
2022-08-12 14:59:11 -0400

    add third phase

--- anagrams.rb
+++ anagrams.rb
@@ -5,11 +5,11 @@ def first_anagram?(str1, str2)
 end
 
 #phase2
-def anagram?(str1, str2)
+def second_anagram?(str1, str2) #time complexity = n^2
   arr = str2.chars
-  str1.chars.each do |el|
-    if arr.include?(el)
-      index = arr.find_index(el)
+  str1.chars.each do |el| #n
+    if arr.include?(el) #n
+      index = arr.find_index(el) #n but this doesn't change overall time 
       arr.delete_at(index)
     else
       return false
@@ -23,8 +23,13 @@ def anagram?(str1, str2)
   end
 end
 
+#phase 3
+def third_anagram?(str1, str2)
+  arr1 = str1.chars.sort
+  arr2 = str2.chars.sort #sort is n log n
 
-
+  arr1 == arr2
+end
 
 
 
