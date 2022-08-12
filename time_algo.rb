0de13ca7f1a688895d2cba9fbc67e771b5c9c5c3
sungyotkim <sungyotkim@gmail.com>
2022-08-12 13:36:54 -0400

    finish phase 2

--- time_algo.rb
+++ time_algo.rb
@@ -6,5 +6,14 @@ def my_min_phase_1(arr)
 end
 
 list = [ 0, 3, 5, 4, -5, 10, 1, 90]
-p my_min_phase_1(list)  # =>  -5
+# p my_min_phase_1(list)  # =>  -5
+
+def my_min_phase_2(arr)
+  min = arr[0]
+  arr.each do |num|
+    min = num if num < min
+  end
+  min
+end
+
 # p my_min_phase_2(list)  # =>  -5
